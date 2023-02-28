f643043dbb0330085546426b9336e310bf4b1cac
Bartosz Lewandowski <blewandowski.2221@gmail.com>
2023-02-28 17:07:30 +0100

    updated env

--- src/components/lib/axios/useGet.ts
+++ src/components/lib/axios/useGet.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function useGet(table: string) {
-  const { data } = await axios.get(`http://localhost:3000/api/${table}`);
+  const { data } = await axios.get(`https://11-beige.vercel.app/api/${table}`);
 
   return data;
 }
